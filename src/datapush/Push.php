1dcbb5dac9a108707353e3db699babe876eb0e91
leijia <584839656@qq.com>
2020-10-13 10:58:40 +0800

    优化post提交延迟问题

--- src/datapush/Push.php
+++ src/datapush/Push.php
@@ -231,12 +231,16 @@ class Push
         }
         $postUrl = $url;
         $curlPost = $param;
+        if(is_array($curlPost)){
+            $curlPost = http_build_query($curlPost);
+        }
         $ch = curl_init();//初始化curl
         curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
         curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+        curl_setopt($ch, CURLOPT_ENCODING, "gzip");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
         $data = curl_exec($ch);//运行curl
         curl_close($ch);
